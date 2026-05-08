ed5fd6b05bf92ce96a8484896ca1940579b4a67a
burner087513135 <lucky.the.cat.mizzi@gmail.com>
2026-05-08 20:01:26 +0200

    Update spots.js

--- spots.js
+++ spots.js
@@ -4,22 +4,52 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19
 }).addTo(map);
 
-fetch("./locations.json")
-  .then(res => res.json())
-  .then(spots => {
-    spots.forEach(spot => {
-      const popupContent = `
-        <b>${spot.name}</b><br>
-        ${spot.description}<br><br>
-        <a href="${spot.maps_link}" target="_blank">Open in Google Maps</a>
-      `;
+let allSpots = [];
+let hiddenUnlocked = false;
 
-      L.marker([spot.lat, spot.lng])
-        .addTo(map)
-        .bindPopup(popupContent);
+function loadSpots() {
+  fetch("./locations.json")
+    .then(res => res.json())
+    .then(data => {
+      allSpots = data;
+      renderSpots();
     });
-  })
-  .catch(err => {
-    alert("Failed to load locations.json");
-    console.error(err);
+}
+
+function renderSpots() {
+  // clear map
+  map.eachLayer(layer => {
+    if (layer instanceof L.Marker) {
+      map.removeLayer(layer);
+    }
   });
+
+  allSpots.forEach(spot => {
+    if (spot.hidden && !hiddenUnlocked) return;
+
+    const popup = `
+      <b>${spot.name}</b><br>
+      ${spot.description}<br><br>
+      <a href="${spot.maps_link}" target="_blank">Open in Maps</a>
+    `;
+
+    L.marker([spot.lat, spot.lng])
+      .addTo(map)
+      .bindPopup(popup);
+  });
+}
+
+// button logic
+document.getElementById("revealBtn").addEventListener("click", () => {
+  const password = prompt("Enter password:");
+
+  if (password === "myfavoritecolourisPURPLE") {
+    hiddenUnlocked = true;
+    renderSpots();
+    alert("Hidden spots unlocked");
+  } else {
+    alert("Wrong password");
+  }
+});
+
+loadSpots();
